cc3dc5c60df045b6235db11d5ff45b663f1901f7
Itou <a1929776588@gmail.com>
2023-04-19 14:57:47 +0900

    https://paiza.jp/works/mondai/stack_queue/stack_queue__common_step1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        List<int> list = new List<int>();
+        for (int n = 0; n < num; n++)
+        {
+            list.Add(int.Parse(Console.ReadLine()));
+        }
+        Console.WriteLine(num);
+        for (int n = 0; n < num; n++)
+        {
+            Console.WriteLine(list[n]);
+        }
+    }
+}
